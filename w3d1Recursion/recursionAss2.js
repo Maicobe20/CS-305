67a01a1db6f30aefc41776066d44fd92deb2f8f8
Maicobe20 <mikeberhane2020@gmail.com>
2021-09-15 02:54:46 -0500

    Recursion assignment

---
+++ w3d1Recursion/recursionAss2.js
@@ -0,0 +1,100 @@
+function TreeNode(value) {
+    this.value = value;
+    this.descendents = [];
+}
+// create nodes with values
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+// associate root with is descendents
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
+
+// //    abe={value:"Abe",
+//         descendents:[{value:'Homer',
+//                       descendents:[{value:'Bart'},
+//                                    {value:'Lisa'},
+//                                    {value:'Maggie'}]}]}
+
+
+//Q1
+   function listNames(tree){
+       for(let key in tree){
+           if(Array.isArray(tree[key])){
+               for(let key2 of tree[key]){
+                   listNames(key2)
+               }
+           }else{
+               console.log(tree.value)
+           }
+
+       }
+   }
+   listNames(abe)
+
+
+   //Q2
+
+let mike=[]
+   function listNames(tree,name){
+    for(let key in tree){
+        if(Array.isArray(tree[key])){
+            for(let key2 of tree[key]){
+                listNames(key2)
+            }
+        }else{
+            mike.push(tree.value)
+
+            }
+
+
+    }
+
+    for(let item of mike){
+        if(item == name){
+            return true
+        }else{
+            return false
+        }
+
+    }
+
+   }
+console.log(listNames(abe,'Lisa'))
+
+
+//Q3
+
+function findSubtree(tree, name) {
+    
+    for (let key in tree) {
+    
+            if (Array.isArray(tree[key])) {
+                for (let key2 of tree[key]) {
+                    findSubtree(key2)
+                }
+            } else {
+                console.log(tree.value)
+            }
+
+    }
+}
+findSubtree(abe)
+
+
+// Q4
+
+let list = { name: "Abe" };
+list.next = { name: "Homer" };
+list.next.next = { name: "Bart" };
+list.next.next.next = { name: "Lisa" };
+list.next.next.next.next = { name: "Maggie" };
+
+console.log(list);
+
+
+
+
+
---
+++ w3d1Recursion/w3d1Recursion.js
@@ -0,0 +1,167 @@
+"use strict";
+
+/* global exports */
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
+*/
+module.exports = { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop}; //add all of your function names here that you need for the node mocha tests
+
+
+//Q1
+function sumTo(n){
+    if(n===1){
+        return 1
+    }else{
+        return n+sumTo(n-1)
+    }
+}
+console.log(sumTo(5))
+
+//Q2
+
+function factorial(n){
+    if(n===1){
+        return 1
+    }else{
+        return n * factorial(n-1)
+    }
+}
+console.log(factorial(5))
+
+//Q3
+
+function fibonacci(n){
+    if (n<=1){
+        return n
+    }else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+console.log(fibonacci(10))
+
+//Q4
+
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+function outputListLoop(list){
+  let x = " "
+   do{
+       x = x + ' ' + list.value 
+       list = list.next
+   } while(list)
+   console.log(x + " printed to console")
+}
+
+outputListLoop(list)
+
+function outputList(list){
+    console.log(list.value)
+    if(list.next){
+        outputList(list.next)
+    }
+}
+
+
+outputList(list)
+
+
+function reverseList(list){
+    if(list.next){
+        reverseList(list.next)
+    }
+    console.log(list.value)
+}
+
+reverseList(list)
+
+function reverseListLoop(list){
+    let mike = []
+
+    while(list){
+        mike.push(list.value)
+        list = list.next
+    }
+    for (let i = (mike.length - 1);i<=0;i--){
+        console.log(mike[i])
+    }
+    
+
+}
+reverseListLoop(list)
+
+let node3= {
+    name: "p",
+    value: "This is text in the a paragraph",
+    children: null
+};
+ let node4= {
+    name: "label",
+    value: "Name",
+    children: null
+};
+ let node5= {
+    name: "input",
+    value: "this was typed by a user",
+    children: null
+};
+let node2= {
+    name: "div",
+    value: null,
+    children: [node4, node5]
+};
+ let node1= {
+    name: "body",
+    children: [node2, node3],
+    value: null,
+};
+
+
+
+function printNameValue(mike) {
+    
+    for (let key in mike) {
+        if (Array.isArray(mike[key])) {
+            for (let key2 of mike[key]) {
+                printNameValue(key2)
+            }
+
+        } else {
+            console.log(mike.name + ": " + mike.value)
+        }
+    }
+    
+}
+printNameValue(node1)
+
+
+
+
+   let myArray =[]
+  
+      function nameValue(node) {
+    
+    for (let key in node) {
+        if (Array.isArray(node[key])) {
+            for (let key2 of node[key]) {
+                nameValue(key2)
+            }
+
+        } else {
+            myArray.push(node.name + ": " + node.value)
+        }
+    }
+    return myArray
+}
+
+
+console.log(nameValue(node1))
